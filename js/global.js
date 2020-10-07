d78a5c3f58bdfa04ff2a604810c70f48de9dec99
Daniel M. N <junem3ta@gmail.com>
2020-10-07 21:39:50 +0300

    Add Account Recovery UI

--- js/global.js
+++ js/global.js
@@ -11,11 +11,14 @@ $(document).ready(() => {
         lnEl = $('.c-u-ln'),
         cuunEl = $('.c-u-un'),
         emailEl = $('.c-u-email'),
+        /* Recover Account fields, RAF */
+        rAEmail = $('.r-a-email'),
         markAsErroneous = (el) => {
             el.closest('div').addClass('aux-input-error');
         },
         priorLCErrors = false,
-        priorRFErrors = false;
+        priorRFErrors = false,
+        priorRAFErrors = false;
     const
         isAN /* AlphaNumeric Test */ = /^[a-zA-Z0-9]+$/,
         isText = /^[a-zA-Z']+$/,
@@ -54,6 +57,15 @@ $(document).ready(() => {
             priorRFErrors = valid ? false : true;
             return valid ? true : false;
         },
+        verifyRAF = (email) => {
+            let valid = true;
+            if(email == '' || !rfc2822Email.test(email)) {
+                markAsErroneous(rAEmail);
+                priorRAFErrors = true;
+                return false;
+            }   
+            return true;
+        },
         resetErrNotification = (elements) => {
             for(let i=0; i<elements.length; i++) {
                 if(elements[i].closest('div').hasClass('aux-input-error')) {
@@ -61,18 +73,24 @@ $(document).ready(() => {
                 }
             }
         };
-    $('.v-u').click(()=> {
+    $('.v-u').click(() => {
         priorLCErrors ? resetErrNotification([unEl, pwEl]) : '';
         if(verifyLC($.trim(unEl.val()), $.trim(pwEl.val()))) {
             breadcrumbs = {
                 "un": $.trim(unEl.val()),
                 "pw": $.trim(pwEl.val())
             }
+            $('.v-u').prop('disabled', true);
+            $('.v-u-log span.p-h').text('Operation in progress');
+            $('.v-u-log').addClass('bg-loading');
             /* Ajax Call here */
+            setTimeout(() => {
+                $('.v-u').prop('disabled', false);
+            }, 5000);
         }
         return false;
     });
-    $('.c-u').click(()=> {
+    $('.c-u').click(() => {
         priorRFErrors ? resetErrNotification([fnEl, lnEl, cuunEl, emailEl]) : '';
         if(verifyRF($.trim(fnEl.val()), $.trim(lnEl.val()), $.trim(cuunEl.val()), $.trim(emailEl.val()))) {
             breadcrumbs = {
@@ -81,7 +99,31 @@ $(document).ready(() => {
                 "un": $.trim(cuunEl.val()),
                 "email": $.trim(emailEl.val()),
             }
+            $('.c-u').prop('disabled', true);
+            $('.c-u-log span.p-h').text('Operation in progress');
+            $('.c-u-log').addClass('bg-loading');
             /* Ajax Call here */
+            setTimeout(() => {
+                $('.c-u').prop('disabled', false);
+            }, 5000);
+        }
+        return false;
+    });
+    $('.r-a').click(() => {
+        priorRAFErrors ? resetErrNotification([rAEmail]) : '';
+        _l(0);
+        if(verifyRAF($.trim(rAEmail.val()))) {
+            _l(1);
+            breadcrumbs = {
+                "email": $.trim(rAEmail.val()),
+            }
+            $('.r-a').prop('disabled', true);
+            $('.r-a-log span.p-h').text('Operation in progress');
+            $('.r-a-log').addClass('bg-loading');
+            /* Ajax Call here */
+            setTimeout(() => {
+                $('.r-a').prop('disabled', false);
+            }, 5000);
         }
         return false;
     });
